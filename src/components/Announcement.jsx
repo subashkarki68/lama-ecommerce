c28f3bc5e55f4d9be31729c364413da1f2b198ef
subash karki <subashkarki68@gmail.com>
2022-07-24 11:59:17 +0545

    announcement close feature

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components"
+import CloseIcon from '@mui/icons-material/Close';
+import { useState } from "react";
+
 
 const Container = styled.div`
     background-color: #1B9CFC;
@@ -7,13 +10,29 @@ const Container = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    // if close button is clicked then display none
+    display: ${(props) => props.closeAnnouncement && "none"};
+`;
+const SuperText = styled.span`
+    flex: 1;
+    text-align: center;
+`;
+const CloseButton = styled(CloseIcon)`
+    color: #fff;
+    justify-content: flex-end;
+    align-items: flex-end;
+    cursor: pointer;
 `;
 
 const Announcement = () => {
+    //This state is used to close the announcement if close button is clicked
+    const [isAnnouncementClosed, setAnnouncementClosed] = useState(false);
+
     return (
         <div>
-            <Container>
-                Super Deal, get 50% off on all purchase.
+            <Container closeAnnouncement={isAnnouncementClosed}>
+                <SuperText>Super Deal, get 50% off on all purchase.</SuperText>
+                <CloseButton onClick={() => setAnnouncementClosed(true)} />
             </Container>
         </div>
     )
